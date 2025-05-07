f94061354549e34499b6bf7a8213243cb898e866
TheCharm2003 <chirayuagrawal454@gmail.com>
2025-05-07 08:21:06 +0530

    Config

--- truffle-config.js
+++ truffle-config.js
@@ -65,9 +65,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
+     host: "0.0.0.0",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "*",      // Any network (default: none)
     },
     //
     // An additional network, but with some advanced options…
@@ -108,13 +108,13 @@ module.exports = {
     solc: {
       version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
       //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+       },
+       evmVersion: "byzantium"
+      }
     }
   },
 
